9583b2f9ddd54c42c749a283aad73a00689926cd
Mark Headd <mheadd@voiceingov.org>
2011-07-07 14:46:47 -0400

    Added headers parameter on transfer. New transfer sample script.

---
+++ samples/transfer.js
@@ -0,0 +1,21 @@
+/**
+ * A very simple node web server that will respond to requests
+ *  with a transfer to a SIP or PSTN number.
+ */
+
+var http = require('http');
+require('../lib/tropo-webapi');
+
+var server = http.createServer(function (request, response) {
+	
+	// Create a new instance of the TropoWebAPI object.
+	var tropo = new TropoWebAPI();
+	
+	tropo.say('Please hold while your call is transferred.');
+	tropo.transfer('mheadd@sip2sip.info', false, null, null, {'x-caller-name' : 'Mark Headd'}, null, null, true, '#', 60.0);
+	
+	// Render out the JSON for Tropo to consume.
+	response.writeHead(200, {'Content-Type': 'application/json'}); 
+	response.end(TropoJSON(tropo));
+	
+}).listen(8000); // Listen on port 8000 for requests.
