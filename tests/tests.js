8347db31c40a6eb392977d1272e57e85b77dce25
pengxli <pengxli@cisco.com>
2017-12-04 18:09:28 +0800

    bug TROPO-12507 tropo-webapi-node record

--- tests/tests.js
+++ tests/tests.js
@@ -14,7 +14,7 @@ var callExpected = '{"tropo":[{"call":{"to":"3055195825","name":"call"}}]}';
 var conferenceExpected = '{"tropo":[{"conference":{"id":"1234","name":"foo","mute":false,"playTones":false,"terminator":"#"}}]}';
 var hangupExpected = '{"tropo":[{"hangup":{}}]}';
 var messageExpected = '{"tropo":[{"message":{"say":{"value":"This is an announcement"},"to":"3055195825","name":"message","answerOnMedia":false,"channel":"TEXT","from":"3055551212","network":"SMS","timeout":10,"voice":"kate"}}]}';
-var recordExpected = '{"tropo":[{"record":{"beep":true,"choices":{"value":"[5 DIGITS]"},"maxSilence":5,"method":"POST","name":"foo","required":true,"say":{"value":"Please say your account number"}}}]}';
+var recordExpected = '{"tropo":[{"record":{"beep":true,"choices":{"terminator":"#"},"maxSilence":5,"method":"POST","name":"foo","required":true,"say":{"value":"Please say your account number"}}}]}';
 
 var numTests = 0;
 var numFailed = 0;
@@ -92,7 +92,7 @@ function recordTest(expected) {
 
 	var tropo = new TropoWebAPI();
 	var say = new Say("Please say your account number");
-	var choices = new Choices("[5 DIGITS]");
+	var choices = new Choices("[5 DIGITS]", null, "#");
 	tropo.record(null, null, null, true, choices, null, 5, null, "POST", null, "foo", true, say);
 	return runTest(TropoJSON(tropo), expected);
 
